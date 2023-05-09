052653a3d3fac8c397444ba0ddb6c7d48a1ec3ee
Nick Valdez <nicholastvaldez@gmail.com>
2023-05-09 13:40:59 -0500

    format prices

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image"
+import formatPrice from "@/util/PriceFormat"
 
-export default function Product({ name, image, price }) {
+import { ProductType } from "@/types/ProductType"
+
+export default function Product({ name, image, price }: ProductType) {
   return (
     <div>
       <Image src={image} alt={name} width={400} height={400} />
       <h1>{name}</h1>
-      {price}
+      {price !== null ? formatPrice(price) : "NA"}
     </div>
   )
 }
